675eaf085d0b6062e1ee94e0ec03171f37ad9e0e
Lidskiy1704 <fedorenko.matvey.s@gmail.com>
2023-10-11 19:25:01 +0300

    refactored.

---
+++ Calculator.cs
@@ -0,0 +1,91 @@
+﻿namespace CalculatorTMS;
+
+internal class Calculator
+{
+    public static double ReadFirstNumber()
+    {
+        double firstNumber;
+
+        do
+        {
+            Console.WriteLine("Введите первое число:");
+        } while (!double.TryParse(Console.ReadLine(), out firstNumber));
+
+        return firstNumber;
+    }
+
+    public static double ReadSecondNumber()
+    {
+        double secondNumber;
+
+        do
+        {
+            Console.WriteLine("Введите второе число:");
+        } while (!double.TryParse(Console.ReadLine(), out secondNumber));
+
+        return secondNumber;
+    }
+
+    public static string ReadSymbol()
+    {
+        string symbol;
+
+        do
+        {
+            Console.WriteLine("Введите математический символ (+, -, *, /, ^):");
+            symbol = Console.ReadLine();
+        } while (!IsMathSymbol(symbol));
+
+        return symbol;
+    }
+
+    public static double? Calculate(double firstNumber, string symbol, double secondNumber)
+    {
+        switch (symbol)
+        {
+            case "+":
+                {
+                    return firstNumber + secondNumber;
+                }
+
+            case "-":
+                {
+                    return firstNumber - secondNumber;
+                }
+
+            case "*":
+                {
+                    return firstNumber * secondNumber;
+                }
+
+            case "/":
+                {
+                    if (secondNumber == 0)
+                    {
+                        Console.WriteLine("На 0 делить нельзя! Соберись и давай с самого начала)");
+                        return null;
+                    }
+
+                    return firstNumber / secondNumber;
+                }
+
+            case "^":
+                {
+                    return Math.Pow(firstNumber, secondNumber);
+                }
+
+            default:
+                return null;
+        }
+    }
+
+    private static bool IsMathSymbol(string symbol)
+    {
+        if (string.IsNullOrWhiteSpace(symbol))
+        {
+            return false;
+        }
+
+        return symbol == "+" || symbol == "-" || symbol == "*" || symbol == "/" || symbol == "^";
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,67 +4,11 @@ internal class Program
 {
     static void Main(string[] args)
     {
-        double firstNumber;
-        double secondNumber;
-        string? symbol;
+        var firstNumber = Calculator.ReadFirstNumber();
+        var symbol = Calculator.ReadSymbol();
+        var secondNumber = Calculator.ReadSecondNumber();
+        var result = Calculator.Calculate(firstNumber, symbol, secondNumber);
 
-        do
-        {
-            Console.WriteLine("Введите первое число:");
-        } while (!double.TryParse(Console.ReadLine(), out firstNumber));
-
-        do
-        {
-            Console.WriteLine("Введите математический символ (+, -, *, /, ^):");
-            symbol = Console.ReadLine();
-        } while (string.IsNullOrWhiteSpace(symbol) || (symbol != "+" && symbol != "-" && symbol != "*" && symbol != "/" && symbol != "^"));
-
-        do
-        {
-            Console.WriteLine("Введите второе число:");
-        } while (!double.TryParse(Console.ReadLine(), out secondNumber));
-
-
-        switch (symbol)
-        {
-            case "+":
-                {
-                    var result = firstNumber + secondNumber;
-                    Console.WriteLine(result);
-                }
-                break;
-
-            case "-":
-                {
-                    var result = firstNumber - secondNumber;
-                    Console.WriteLine(result);
-                }
-                break;
-            case "*":
-                {
-                    var result = firstNumber * secondNumber;
-                    Console.WriteLine(result);
-                }
-                break;
-            case "/":
-                {
-                    if (secondNumber == 0)
-                    {
-                        Console.WriteLine("На 0 делить нельзя! Соберись и давай с самого начала)");
-                    }
-                    else
-                    {
-                        var result = firstNumber / secondNumber;
-                        Console.WriteLine(result);
-                    }
-                }
-                break;
-            case "^":
-                {
-                    var result = Math.Pow(firstNumber, secondNumber);
-                    Console.WriteLine(result);
-                }
-                break;
-        }
+        Console.WriteLine(result);
     }
 }
